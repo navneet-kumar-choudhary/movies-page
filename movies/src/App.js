7a0d5b0c6d0764999daa82b6db72d8f5bbb7aec9
Navneet Kumar Choudhary <navneetchydbg@gmail.com>
2021-06-16 17:27:31 +0530

    added multiple components

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MoviesPage from "./component/MoviesPage";
-// import MoviePage2 from "./component/MoviePage2";
+// import MoviesPage from "./component/MoviesPage";
+import MoviePage_II from "./component/MoviePage_II";
 import New from "./component/New";
 function App() {
     return (
         <>
             <New></New>
-            <MoviesPage></MoviesPage>,{/* <MoviePage2></MoviePage2>  */}
+            {/* <MoviesPage></MoviesPage>, */}
+            <MoviePage_II></MoviePage_II>
         </>
     );
 }
